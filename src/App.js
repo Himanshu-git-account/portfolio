551e4883690e1df35b439643e152edfa39f8801d
Himanshu-git-account <khimanshu0507@gmail.com>
2021-04-17 01:07:46 +0530

    added Google analytics

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React, { useEffect } from 'react';
 import Header from './Component/Header/Header';
 import AboutMe from './Component/AboutMe';
 import TimeLines from './Component/TimeLines';
@@ -6,10 +7,16 @@ import Footer from './Component/Footer';
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import ReactGA from 'react-ga';
 
 library.add(fab, faCheckSquare, faCoffee)
 
 function App() {
+  useEffect(() => {
+    ReactGA.initialize('UA-194780690-1');
+    // To Report Page View 
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, [])
   return (
     <div className="App">
       <Header />
